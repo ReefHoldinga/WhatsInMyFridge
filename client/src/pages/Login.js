0370b63f40614b7dffd9a1950fd58c071819239d
Reef Holdinga <reefholdinga@gmail.com>
2023-12-22 11:16:27 -0500

    Added complete login functionality and error message

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import "./styles/Login.css";
 
+const errorMsg = document.getElementsByClassName('error');
+
 const Login = () => {
   useEffect(() => {
     const labels = document.querySelectorAll('.form-body label');
@@ -28,22 +30,57 @@ const Login = () => {
     }
   }, []);
 
-  useEffect(() => {
-    
-  })
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const user = {email, password};
+
+    const response = await fetch('api/users/login', {
+      method: 'POST',
+      body: JSON.stringify(user),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const json = await response.json();
+
+    if(!response.ok) {
+      setError(json);
+      console.log(error);
+      errorMsg[0].style.visibility = "visible";
+      errorMsg[0].style.position = "relative";
+    }
+
+    window.location.replace("http://localhost:3000/home");
+  };
+  
   return (
     <div className="container">
       <h1>Sign In</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-body">
-          <input type="text" required />
+          <input 
+            type="email" 
+            required 
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <label>Email</label>
         </div>
         <div className="form-body">
-          <input type="password" required />
+          <input 
+            type="password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <label>Password</label>
         </div>
+
+        <p className="error">Incorrect email or password. <a href="">Reset here.</a></p>
 
         <button className="btn">Login</button>
         <p className="text">Don't have an account? <a href="/register">Register here!</a></p>
