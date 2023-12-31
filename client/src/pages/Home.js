194b3c59e0d9584a9df44a7071a577bee47ad0e0
Reef Holdinga <reefholdinga@gmail.com>
2023-12-30 22:48:51 -0500

    Added basic selectors to home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import "./styles/Home.css";
 
+const categories = document.querySelectorAll('storage-container');
+
 const Home = () => {
   useEffect(()=> {
     // Add class to the body element
@@ -13,71 +15,71 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="home-container">
-      <nav className="navbar">
-        <ul>
-          <li><h1>Hello Reef!</h1></li>
-          <li>
-            X items Expiring Soon!
-            <ol className="click-me"> Click to view expiring soon.</ol>
-          </li>
-          <li>
-            Running Low on X Items!
-            <ol className="click-me">Click to view items that are running low.</ol>
-          </li> 
-          <li className="settings">
-            <button>Settings</button>
-          </li>
-          <li className="logout">
-            <button>Logout</button>
-          </li>
-        </ul>
-      </nav>
-      <main className="main-body">
-        <div className="item-container">
-          <h2>Peanut Butter</h2>
-          <p>Expires 10/25</p>
-          <p>Section: Pantry</p>
-          <p>Amount: 1 jar</p>
-        </div>
-
-        <div className="item-container">
-          <h2>Peanut Butter</h2>
-          <p>Expires 10/25</p>
-          <p>Section: Pantry</p>
-          <p>Amount: 1 jar</p>
-        </div>
-
-        <div className="item-container">
-          <h2>Peanut Butter</h2>
-          <p>Expires 10/25</p>
-          <p>Section: Pantry</p>
-          <p>Amount: 1 jar</p>
-        </div>
-
-        <div className="item-container">
-          <h2>Peanut Butter</h2>
-          <p>Expires 10/25</p>
-          <p>Section: Pantry</p>
-          <p>Amount: 1 jar</p>
-        </div>
-
-      </main>
-      <div className="storage-container">
-        <aside className="storage">
-          <h2>Categories</h2>
-        </aside>
-        <aside className="storage inactive">
-          <h2>Fridge</h2>
-        </aside>
-        <aside className="storage inactive">
-          <h2>Pantry</h2>
-        </aside>
-        <aside className="storage inactive">
-          <h2>Freezer</h2>
-        </aside>
+    <div class="home-container">
+    <nav class="navbar">
+      <ul>
+        <li><h1>Hello Reef!</h1></li>
+        <li>
+          X items Expiring Soon!
+          <ol class="click-me"> Click to view expiring soon.</ol>
+        </li>
+        <li>
+          Running Low on X Items!
+          <ol class="click-me">Click to view items that are running low.</ol>
+        </li> 
+        <li class="settings">
+          <button>Settings</button>
+        </li>
+        <li class="logout">
+          <button>Logout</button>
+        </li>
+      </ul>
+    </nav>
+    <main class="main-body">
+      <div class="item-container">
+        <h2>Peanut Butter</h2>
+        <p>Expires 10/25</p>
+        <p>Section: Pantry</p>
+        <p>Amount: 1 jar</p>
       </div>
-    </div>  
+
+      <div class="item-container">
+        <h2>Peanut Butter</h2>
+        <p>Expires 10/25</p>
+        <p>Section: Pantry</p>
+        <p>Amount: 1 jar</p>
+      </div>
+
+      <div class="item-container">
+        <h2>Peanut Butter</h2>
+        <p>Expires 10/25</p>
+        <p>Section: Pantry</p>
+        <p>Amount: 1 jar</p>
+      </div>
+
+      <div class="item-container">
+        <h2>Peanut Butter</h2>
+        <p>Expires 10/25</p>
+        <p>Section: Pantry</p>
+        <p>Amount: 1 jar</p>
+      </div>
+
+    </main>
+    <div class="storage-container">
+      <aside class="storage">
+        <h2>Categories</h2>
+      </aside>
+      <aside class="storage inactive">
+        <h2>Fridge</h2>
+      </aside>
+      <aside class="storage inactive">
+        <h2>Pantry</h2>
+      </aside>
+      <aside class="storage inactive">
+        <h2>Freezer</h2>
+      </aside>
+    </div>
+  </div>  
   )
 }
 
